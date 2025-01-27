9e3bfb0addf265637146abdf9a64b4f209b3edeb
Michael Wasney <70529918+michaelwasney@users.noreply.github.com>
2025-01-27 15:49:40 -0800

    Update strain_inference.py

--- scripts/strain_inference/strain_inference.py
+++ scripts/strain_inference/strain_inference.py
@@ -433,95 +433,300 @@ if len(final_clusters) > 0:
         final_f[i] = final_f[i][mask]
     Fs = Fs.loc[:,mask]
 
-#Creating output for R plotting (with bootstrapped 95% confidence intervals
-bootstrap_ci = True
-boostrap_k = 100
-bootstrap_N = 1000
-for i in np.arange(len(final_f)): 
-    if i == 0:
-        final_f_all_strains = pd.DataFrame(final_f[i]).reset_index().rename(columns = {0: "freq"})
-        final_f_all_strains['species'] = [species]*len(final_f_all_strains)
-        final_f_all_strains['strain'] = [i + 1]*len(final_f_all_strains)
-        final_f_all_strains = final_f_all_strains[['species','strain','mouse_number', 'region', 'diet', 'cage', 'sample', 'freq']]
-        iqr_75 = pd.DataFrame(final_clusters[i].quantile(0.75)).reset_index()
-        iqr_25 = pd.DataFrame(final_clusters[i].quantile(0.25)).reset_index()
-        final_f_all_strains = final_f_all_strains.merge(iqr_25).merge(iqr_75)
-        if bootstrap_ci:
-            upper_ci_vec = []
-            lower_ci_vec = []
-            
-            for sample_i in range(final_clusters[i].shape[1]):
-                
-                sample_name = final_clusters[i].iloc[:,sample_i].name[4]
-                if  (no_cluster) & (multiple_inoculum_strains) & (sample_name != "TL1gDNAshort"):
-                    upper_ci_vec.append(np.nan)
-                    lower_ci_vec.append(np.nan)
-                
-                elif (no_cluster) & (not multiple_inoculum_strains):
-                    upper_ci_vec.append(np.nan)
-                    lower_ci_vec.append(np.nan)
-                    
-                else:
-                    snv_freqs = final_clusters[i].iloc[:,sample_i].dropna().to_list()
-                    mean_freq_vec = []
-                    for n in range(bootstrap_N):
-                        sampled_snv_freqs = choices(snv_freqs, k = boostrap_k)
-                        mean_freq = np.mean(sampled_snv_freqs)
-                        mean_freq_vec.append(mean_freq)
-                    upper_ci = np.quantile(mean_freq_vec, 0.975)
-                    lower_ci = np.quantile(mean_freq_vec, 0.025)
-                    upper_ci_vec.append(upper_ci)
-                    lower_ci_vec.append(lower_ci)
-                
-            
+if not single_trajectory:
+  #Creating output for R plotting (with bootstrapped 95% confidence intervals
+  bootstrap_ci = True
+  boostrap_k = 100
+  bootstrap_N = 1000
+  for i in np.arange(len(final_f)): 
+      if i == 0:
+          final_f_all_strains = pd.DataFrame(final_f[i]).reset_index().rename(columns = {0: "freq"})
+          final_f_all_strains['species'] = [species]*len(final_f_all_strains)
+          final_f_all_strains['strain'] = [i + 1]*len(final_f_all_strains)
+          final_f_all_strains = final_f_all_strains[['species','strain','mouse_number', 'region', 'diet', 'cage', 'sample', 'freq']]
+          iqr_75 = pd.DataFrame(final_clusters[i].quantile(0.75)).reset_index()
+          iqr_25 = pd.DataFrame(final_clusters[i].quantile(0.25)).reset_index()
+          final_f_all_strains = final_f_all_strains.merge(iqr_25).merge(iqr_75)
+          if bootstrap_ci:
+              upper_ci_vec = []
+              lower_ci_vec = []
+              
+              for sample_i in range(final_clusters[i].shape[1]):
+                  
+                  sample_name = final_clusters[i].iloc[:,sample_i].name[4]
+                  if  (no_cluster) & (multiple_inoculum_strains) & (sample_name != "TL1gDNAshort"):
+                      upper_ci_vec.append(np.nan)
+                      lower_ci_vec.append(np.nan)
+                  
+                  elif (no_cluster) & (not multiple_inoculum_strains):
+                      upper_ci_vec.append(np.nan)
+                      lower_ci_vec.append(np.nan)
+                      
+                  else:
+                      snv_freqs = final_clusters[i].iloc[:,sample_i].dropna().to_list()
+                      mean_freq_vec = []
+                      for n in range(bootstrap_N):
+                          sampled_snv_freqs = choices(snv_freqs, k = boostrap_k)
+                          mean_freq = np.mean(sampled_snv_freqs)
+                          mean_freq_vec.append(mean_freq)
+                      upper_ci = np.quantile(mean_freq_vec, 0.975)
+                      lower_ci = np.quantile(mean_freq_vec, 0.025)
+                      upper_ci_vec.append(upper_ci)
+                      lower_ci_vec.append(lower_ci)
+                  
+              
+  
+      else:
+  
+          final_f_all_strains_temp = pd.DataFrame(final_f[i]).reset_index().rename(columns = {0: "freq"})
+          final_f_all_strains_temp['species'] = [species]*len(final_f_all_strains_temp)
+          final_f_all_strains_temp['strain'] = [i + 1]*len(final_f_all_strains_temp)
+          final_f_all_strains_temp = final_f_all_strains_temp[['species','strain','mouse_number', 'region', 'diet', 'cage', 'sample', 'freq']]
+          iqr_75 = pd.DataFrame(final_clusters[i].quantile(0.75)).reset_index()
+          iqr_25 = pd.DataFrame(final_clusters[i].quantile(0.25)).reset_index()
+          final_f_all_strains_temp = final_f_all_strains_temp.merge(iqr_25).merge(iqr_75)
+          final_f_all_strains = final_f_all_strains.append(final_f_all_strains_temp, ignore_index = True)
+          if bootstrap_ci:
+              
+              for sample_i in range(final_clusters[i].shape[1]): #if we did not infer multiple strains, but there may have been multiple strains in the inoculum, just calculate CI for inoculum
+                  sample_name = final_clusters[i].iloc[:,sample_i].name[4]
+                  if  (no_cluster) & (multiple_inoculum_strains) & (sample_name != "TL1gDNAshort"):
+                      upper_ci_vec.append(np.nan)
+                      lower_ci_vec.append(np.nan)
+                 
+                  elif (no_cluster) & (not multiple_inoculum_strains):
+                      upper_ci_vec.append(np.nan)
+                      lower_ci_vec.append(np.nan)
+                      
+                  else:
+                      snv_freqs = final_clusters[i].iloc[:,sample_i].dropna().to_list()
+                      mean_freq_vec = []
+                      for n in range(bootstrap_N):
+                          sampled_snv_freqs = choices(snv_freqs, k = boostrap_k)
+                          mean_freq = np.mean(sampled_snv_freqs)
+                          mean_freq_vec.append(mean_freq)
+                      upper_ci = np.quantile(mean_freq_vec, 0.975)
+                      lower_ci = np.quantile(mean_freq_vec, 0.025)
+                      upper_ci_vec.append(upper_ci)
+                      lower_ci_vec.append(lower_ci)
+          
+          
+      final_f_all_strains['upper_ci'] = upper_ci_vec
+      final_f_all_strains['lower_ci'] = lower_ci_vec
+  #Renaming
+  final_f_all_strains = final_f_all_strains.rename(columns = {0.25: "quantile_25", 0.75: "quantile_75"})
+  
+  #Saving 
+  ### Make strain_cluster directory if necessary
+  output_dir = "%sstrain_phasing/strain_clusters/%s/" % (config.project_folder, species)
+  if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
+  ### Saving
+  output_file = "%sstrain_phasing/strain_clusters/%s/%s_strain_frequency.csv" % (config.project_folder, species, species)
+  final_f_all_strains.to_csv(output_file, sep = "\t", index = False)
 
-    else:
+else:
 
-        final_f_all_strains_temp = pd.DataFrame(final_f[i]).reset_index().rename(columns = {0: "freq"})
-        final_f_all_strains_temp['species'] = [species]*len(final_f_all_strains_temp)
-        final_f_all_strains_temp['strain'] = [i + 1]*len(final_f_all_strains_temp)
-        final_f_all_strains_temp = final_f_all_strains_temp[['species','strain','mouse_number', 'region', 'diet', 'cage', 'sample', 'freq']]
-        iqr_75 = pd.DataFrame(final_clusters[i].quantile(0.75)).reset_index()
-        iqr_25 = pd.DataFrame(final_clusters[i].quantile(0.25)).reset_index()
-        final_f_all_strains_temp = final_f_all_strains_temp.merge(iqr_25).merge(iqr_75)
-        final_f_all_strains = final_f_all_strains.append(final_f_all_strains_temp, ignore_index = True)
-        if bootstrap_ci:
-            
-            for sample_i in range(final_clusters[i].shape[1]): #if we did not infer multiple strains, but there may have been multiple strains in the inoculum, just calculate CI for inoculum
-                sample_name = final_clusters[i].iloc[:,sample_i].name[4]
-                if  (no_cluster) & (multiple_inoculum_strains) & (sample_name != "TL1gDNAshort"):
-                    upper_ci_vec.append(np.nan)
-                    lower_ci_vec.append(np.nan)
-               
-                elif (no_cluster) & (not multiple_inoculum_strains):
-                    upper_ci_vec.append(np.nan)
-                    lower_ci_vec.append(np.nan)
-                    
-                else:
-                    snv_freqs = final_clusters[i].iloc[:,sample_i].dropna().to_list()
-                    mean_freq_vec = []
-                    for n in range(bootstrap_N):
-                        sampled_snv_freqs = choices(snv_freqs, k = boostrap_k)
-                        mean_freq = np.mean(sampled_snv_freqs)
-                        mean_freq_vec.append(mean_freq)
-                    upper_ci = np.quantile(mean_freq_vec, 0.975)
-                    lower_ci = np.quantile(mean_freq_vec, 0.025)
-                    upper_ci_vec.append(upper_ci)
-                    lower_ci_vec.append(lower_ci)
-        
-        
-    final_f_all_strains['upper_ci'] = upper_ci_vec
-    final_f_all_strains['lower_ci'] = lower_ci_vec
-#Renaming
-final_f_all_strains = final_f_all_strains.rename(columns = {0.25: "quantile_25", 0.75: "quantile_75"})
+  ## More ordering utilities
+  mnum = list(set(Fs.T.index.get_level_values("mouse_number")))
+  msite = list(set(Fs.T.index.get_level_values("region")))
+  mdiet = list(set(Fs.T.index.get_level_values("diet")))
+  mcage = list(set(Fs.T.index.get_level_values("cage")))
+  
+  mnum_sample_dic = {m:np.argwhere(reorder_sort(Fs.T,"mouse_number").index.get_level_values("mouse_number") == m).ravel() for m in list(set(mnum))}
+  
+  msite_sample_dic = {m:np.argwhere(reorder_sort(Fs.T,"region").index.get_level_values("region") == m).ravel() for m in list(set(msite))}
+  
+  mdiet_sample_dic = {m:np.argwhere(reorder_sort(Fs.T,"diet").index.get_level_values("diet") == m).ravel() for m in list(set(mdiet))}
+  
+  mcage_sample_dic = {m:np.argwhere(reorder_sort(Fs.T,"cage").index.get_level_values("cage") == m).ravel() for m in list(set(mcage))}
+  
+  all_sample_dics = {"diet":mdiet_sample_dic,"region":msite_sample_dic,"mouse_number":mnum_sample_dic,"cage":mcage_sample_dic}
 
-#Saving 
-### Make strain_cluster directory if necessary
-output_dir = "%sstrain_phasing/strain_clusters/%s/" % (config.project_folder, species)
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-### Saving
-output_file = "%sstrain_phasing/strain_clusters/%s/%s_strain_frequency.csv" % (config.project_folder, species, species)
-final_f_all_strains.to_csv(output_file, sep = "\t", index = False)
+  blank = False
+
+  key_to_sort = "mouse_number"
+  secondary_key = "region"
+  
+  
+  cmap_cage = []
+  # colors_library = [(1, 1, 0.8, 0.25), #creamy yellow
+  #                   (0.529, 0.808, 0.922, 0.25), #sky blue
+  #                   (0.529, 0.808, 0.922, 0.25)]
+  #                   #(0.529, 0.808, 0.722, 0.25), #green blue
+  #                   #(0.294, 0.427, 0.804, 0.15)] #purple blue
+  colors_library = [(0.9058823529411765, 0.8823529411764706, 0.9372549019607843, 2/3), #light purple
+                    (0.6666666666666666, 0.596078431372549, 0.6627450980392157, 2/3), #dark purple
+                    (0.9058823529411765, 0.8823529411764706, 0.9372549019607843, 2/3)]
+                    #(0.529, 0.808, 0.722, 0.25), #green blue
+                    #(0.294, 0.427, 0.804, 0.15)] #purple blue
+  if "Cage 1" in mcage_sample_dic:
+  #     cmap_cage.append(colors_library(0))
+      cmap_cage.append(colors_library[0])
+  if "Cage 2" in mcage_sample_dic:
+      cmap_cage.append(colors_library[1])
+      
+  if "Cage 3" in mcage_sample_dic:
+      cmap_cage.append(colors_library[2])
+      
+  
+  cmap_clus = get_cmap(5,name="Set3")
+  manual_cmap_clus = [(0.396078431372549,0.2627450980392157,0.12941176470588237,1),
+                      (0.8235294117647058,0.7058823529411765,0.5490196078431373,1)]
+  
+  fig,ax = plt.subplots(figsize=(16,8))
+  fig.suptitle(fu.get_pretty_species_name(species),size=30)
+  
+  for i,f in enumerate(final_f):
+      if blank:
+              ff = reorder_sort(f,key_to_sort).sort_index(key=lambda x: x.map(order_dict))
+      else:
+          ff = reorder_sort(f,key_to_sort).sort_index(key=lambda x: x.map(order_dict))
+          ax.plot(ff.values,zorder=100,lw=6,color=manual_cmap_clus[i],label=f"Strain {i+1}");
+          ax.plot(ff.values,zorder=80,lw=7,color="k");
+          if len(final_clusters) != 0:
+              ff_c = reorder_sort(final_clusters[i].T,key_to_sort).T.sort_index(key=lambda x: x.map(order_dict),axis=1)
+              ax.plot(ff_c.sample(min(ff_c.shape[0],10000)).T.values,color=manual_cmap_clus[i],alpha=.01)
+          else:
+              for i in np.arange(len([Fs])):
+                  ff_c = reorder_sort((1-Fs).T,key_to_sort).T.sort_index(key=lambda x: x.map(order_dict),axis=1)
+                  ax.plot(ff_c.sample(min(ff_c.shape[0],10000)).T.values,color=manual_cmap_clus[i],alpha=.01)
+  
+  
+  
+  # ff = reorder_sort(final_f[1],key_to_sort).sort_index(key=lambda x: x.map(order_dict))
+  # ax.plot(ff.values,zorder=100,lw=6,color=cmap_clus(1),label=f"Strain {i+1}");
+  # ax.plot(ff.values,zorder=80,lw=7,color="k")
+  # ff_c = reorder_sort(final_clusters[1].T,key_to_sort).T.sort_index(key=lambda x: x.map(order_dict),axis=1)
+  # ax.plot(ff_c.sample(min(ff_c.shape[0],10000)).T.values,color=cmap_clus(1),alpha=.01)
+  
+  major_x = []
+  minor_x = []
+  labels = []
+  
+  second_xlabels = list(ff.index.get_level_values(secondary_key))
+  
+  
+  #Making the vertical lines and labels
+  i = 0
+  for key, item in all_sample_dics[key_to_sort].items():
+      
+      xmin = item.min() 
+      xmax = item.max()
+      
+      for e in item:
+          if key == "Inoculum":
+              ax.axvline(e,color="k",zorder=0,alpha=1)
+          else:
+              ax.axvline(e,color="k",zorder=0,alpha=.5)   
+              
+          ax.text(e, -0.1, abbreviate_gut_site(second_xlabels[e], blank_inoc = True), ha='center',va='top', clip_on=False,size=15, rotation=0)
+          
+      if xmin != xmax:
+          major_x.extend([xmin,(xmax + xmin)/2,xmax])
+          minor_x.append((xmax + xmin)/2)
+          labels.extend(["",key,""])
+      else:
+          major_x.append(xmin)
+          minor_x.append(xmax)
+          labels.extend([key])   
+          
+      i+=1
+      
+      if ("Inoculum" not in all_sample_dics[key_to_sort]) & (max(all_sample_dics[key_to_sort]) == key):
+          ax.vlines(item[0] - 0.5, 0, -0.15, color='black', lw=0.8, clip_on=False, transform=ax.get_xaxis_transform())
+          ax.vlines(item[-1] + 0.5, 0, -0.15, color='black', lw=2, clip_on=False, transform=ax.get_xaxis_transform())
+  
+      elif key == "Inoculum":
+          ax.vlines(item[0] - 0.5, 0, -0.15, color='black', lw=0.8, clip_on=False, transform=ax.get_xaxis_transform())
+      else:
+          ax.vlines(item[0] - 0.5, 0, -0.15, color='black', lw=0.8, clip_on=False, transform=ax.get_xaxis_transform())
+          ax.vlines(item[-1] + 0.5, 0, -0.15, color='black', lw=0.8, clip_on=False, transform=ax.get_xaxis_transform())
+  
+  #Making the vertical colors
+  key_to_sort = "cage"
+  i = 0    
+  for key, item in sorted(all_sample_dics[key_to_sort].items()):
+          
+      xmin = item.min() 
+      xmax = item.max()
+          
+      if (key == max(all_sample_dics[key_to_sort])):
+          ax.vlines(item[0] - 0.5, 0, -0.15, color='black', lw=2, clip_on=False, transform=ax.get_xaxis_transform())
+          if "Inoculum" != max(all_sample_dics[key_to_sort]):
+              ax.vlines(item[-1] + 0.5, 0, -0.15, color='black', lw=2, clip_on=False, transform=ax.get_xaxis_transform())
+              ax.axvspan(xmin - .25,xmax+.25,color=cmap_cage[i]) #,alpha=.2
+          else:
+              ax.axvspan(xmin - .25,xmax+.25,alpha=.2,color='black') 
+  
+          i+=1
+      else:
+          ax.axvspan(xmin - .1,xmax+.1,color=cmap_cage[i]) #alpha=.2,
+          ax.vlines(item[0] - 0.5, 0, -0.15, color='black', lw=2, clip_on=False, transform=ax.get_xaxis_transform())
+          ax.vlines(item[-1] + 0.5, 0, -0.15, color='black', lw=2, clip_on=False, transform=ax.get_xaxis_transform())
+  
+          i+=1
+  
+  
+  ax.set_xticks(major_x)
+  ax.set_xticks(minor_x, minor = True)
+  ax.set_xticklabels([label if label != "Inoculum" else "" for label in labels]);
+  
+  ax.axhline(0,color="grey")
+  ax.axhline(1,color="grey")
+  
+  ax.tick_params(axis = 'x', which = 'major', length=0,labelsize = 20,pad=45)
+  ax.tick_params(axis = 'x', which = 'minor', length = 10,labelsize = 0)
+      
+  ax.set_ylabel("Strain frequency",size=30)
+  ax.set_ylim([-0.05,1.05]);
+  if "Inoculum" in labels:
+      ax.set_xlim([-1,max(major_x)+0.25]);
+      
+  
+  ax.set_xlabel("Sample", size = 30)
+  
+  
+  fig.legend(prop={"size":20});
+  # fig.legend(prop={"size":20}, bbox_to_anchor=(0.85, 0.5));
+  
+          
+  ############ ADDING SFS #################
+  # Create a new axis on the right side
+  if ("Inoculum" in labels):
+  
+      ax_hist = ax.inset_axes([1, 0, 0.1, 1])
+      
+      if len(final_clusters) ==0:
+          hist_data = Fs["Inoculum"].dropna().values
+          ax_hist.hist(hist_data, alpha = 0.5, bins = 25, color = cmap_clus(0), orientation = "horizontal")
+      else:   
+          for i,final_cluster in enumerate(final_clusters):
+              hist_data = final_cluster["Inoculum"].dropna().values
+              ax_hist.hist(hist_data, alpha = 0.5, bins = 25, color = manual_cmap_clus[i], orientation = "horizontal")
+      
+  #     final_cluster = final_clusters[1]
+  #     hist_data = final_cluster["Inoculum"].dropna().values
+  #     ax_hist.hist(hist_data, alpha = 0.5, bins = 25, color = cmap_clus(1), orientation = "horizontal")
+  
+  #     hist_data = Fs["Inoculum"].dropna().values
+  #     ax_hist.hist(hist_data, alpha = 0.5, bins = 25, color = "grey", orientation = "horizontal")
+      
+      ax_hist.invert_yaxis()
+      ax_hist.set_yticks([])
+      ax_hist.set_xticks([])
+      ax_hist.set_frame_on(False)
+      ax_hist.invert_yaxis()
+  
+      # Set the title for the histogram
+      ax_hist.set_xlabel("Inoculum", fontsize = 20, labelpad=17.5)
+  
+  figure_path = "%s%s%s" % (config.project_folder, species,"_single_strain_trajectory.png")
+  fig.savefig(figure_path, facecolor='white', transparent=False, dpi=300,bbox_inches='tight')
+
+  
+
+
+
 
 
