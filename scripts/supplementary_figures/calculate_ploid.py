1ba8ee37502e622c1a028a41d0a2b4a63464d884
Michael Wasney <70529918+michaelwasney@users.noreply.github.com>
2025-01-27 10:45:29 -0800

    Update calculate_ploid.py

--- scripts/supplementary_figures/calculate_ploid.py
+++ scripts/supplementary_figures/calculate_ploid.py
@@ -25,3 +25,39 @@ sys.path.insert(0, "~/Wasney-Briscoe-2024/scripts/supplementary_figures/helper_s
 
 from annotation import *
 
+# Load species
+
+species_list_path = "cat ~/Wasney-Briscoe-2024/metadata/species_snps.txt"
+species_list = subprocess.check_output(species_list_path, shell=True, stderr=subprocess.STDOUT).split("\n")
+species_list = [species for species in species_list if species != ""]
+
+# Calculate haploid samples
+
+ploid_df = pd.DataFrame(columns = ['species', 'sample', 'ploid'])
+
+species_vec = []
+sample_vec = []
+ploid_vec = []
+
+for species in species_list:
+    samples = diversity_utils.calculate_highcoverage_samples(species)
+    haploid_samples = diversity_utils.calculate_haploid_samples(species)
+    for sample in samples:
+        species_vec.append(species)
+        sample_vec.append(sample)
+        if sample in haploid_samples:
+            ploid_vec.append("Haploid")
+        else:
+            ploid_vec.append("Polyploid")
+    
+ploid_df['species'] = species_vec
+ploid_df['sample'] = sample_vec
+ploid_df['ploid'] = ploid_vec
+
+ploid_df['mouse'] = ploid_df['sample'].apply(lambda sample: extract_mouse_number(sample))
+ploid_df['region'] = ploid_df['sample'].apply(lambda sample: extract_region(sample))
+
+# Saving
+
+out_path = "%s%s" % (config.analysis_directory, "metadata/sample_ploid_status.txt")
+ploid_df.to_csv(out_path, index = False)
