fa1f84243bc15988c7b416e4c1868c9132c7849c
Michael Wasney <70529918+michaelwasney@users.noreply.github.com>
2025-03-01 17:06:41 -0800

    Create qsub_single_sample_pi_EXECUTE.sh

---
+++ example_data/qsub_single_sample_pi_EXECUTE.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+workpath=~/Wasney-Briscoe/
+study=SiteDownsampled
+calctype=Schloissnig
+stat=Pi
+covmin=4
+
+./qsub_single_sample_pi.sh ${workpath}metadata/Bacteroides_vulgatus_57955_list.txt ${workpath}metadata/Bacteroides_vulgatus_57955_lengths.txt ${workpath}metadata/accessions.txt $study 1 $calctype $stat $covmin 0 
