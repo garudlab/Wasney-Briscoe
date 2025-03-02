83da6613f5b4b91bc677163324bb2f451750f8c0
Michael Wasney <70529918+michaelwasney@users.noreply.github.com>
2025-03-01 17:11:07 -0800

    Update qsub_single_sample_pi_EXECUTE.sh

--- example_data/qsub_single_sample_pi_EXECUTE.sh
+++ example_data/qsub_single_sample_pi_EXECUTE.sh
@@ -6,4 +6,4 @@ calctype=Schloissnig
 stat=Pi
 covmin=4
 
-./qsub_single_sample_pi.sh ${workpath}metadata/Bacteroides_vulgatus_57955_list.txt ${workpath}metadata/Bacteroides_vulgatus_57955_lengths.txt ${workpath}metadata/accessions.txt $study 1 $calctype $stat $covmin 0 
+${workpath}scripts/pi/StatsPipeline/qsubs/qsub_single_sample_pi.sh ${workpath}metadata/Bacteroides_vulgatus_57955_list.txt ${workpath}metadata/Bacteroides_vulgatus_57955_lengths.txt ${workpath}metadata/accessions.txt $study 1 $calctype $stat $covmin 0 
