8c6ce07f06f23d6593028888732a5951bfc29e46
Michael Wasney <70529918+michaelwasney@users.noreply.github.com>
2025-03-04 17:29:26 -0800

    Update calculate_error_pvalues.py

--- scripts/postprocessing/postprocessing_scripts/calculate_error_pvalues.py
+++ scripts/postprocessing/postprocessing_scripts/calculate_error_pvalues.py
@@ -12,5 +12,5 @@ sys.stderr.write("Calculating pvalues for %s...\n" % species_name)
 
 output_filename = "%ssnps/%s/annotated_snps.txt.bz2" % (parse_midas_data.data_directory, species_name)
 
-os.system('python %spipe_midas_data.py %s | %serror_filtering_cpp_code/annotate_pvalue --disabled | bzip2 -c > %s' % (parse_midas_data.scripts_directory, species_name, parse_midas_data.scripts_directory, output_filename) )  #changed the directory of annotate_pvalue (original script had it in main microbiome_evolution folder
+os.system('python %spostprocessing_scripts/pipe_midas_data.py %s | %spostprocessing_scripts/error_filtering_cpp_code/annotate_pvalue --disabled | bzip2 -c > %s' % (parse_midas_data.scripts_directory, species_name, parse_midas_data.scripts_directory, output_filename) )  #changed the directory of annotate_pvalue (original script had it in main microbiome_evolution folder
  
